90bae6d164951b66a7834c9c05dd773bfd0ff1fc
Risha68 <nimara35-2668@diu.edu.bd>
2021-12-25 01:04:51 +0600

    settings.py updated

--- farmestic/urls.py
+++ farmestic/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 
-admin.site.site_header= "Farmestic Admin Panel"
-admin.site.site_title = "Welcome To Farmestic"
+admin.site.site_header= "FarmerWholesale Admin Panel"
+admin.site.site_title = "Welcome To FarmerWholesale"
 admin.site.index_title = "Dashboard"
 
 urlpatterns = [
